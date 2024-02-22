0f2b10623a8dd2fbe54870ef647e36a62b4ef36c
Tanisha Verma <133028789+Tanisha-Verma-16@users.noreply.github.com>
2024-02-22 23:41:26 +0530

    75

---
+++ SortColors.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++)
+        {
+            boolean b = false;
+            for (int j = 0; j < nums.length - i - 1;j++ )
+            {
+                if (nums[j] > nums[j+1])
+                {
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1]= temp;
+                    b = true;
+                }
+            }
+            if (b == false)
+                {
+                    break;
+                }
+            
+        }
+    }
+}
